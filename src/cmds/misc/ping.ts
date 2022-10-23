5c283ec43664696faba6b67065cb845da3aa2cd3
Vyast <noahbaca2002@icloud.com>
2022-10-22 19:20:29 -0600

    change ping command description

--- src/cmds/misc/ping.ts
+++ src/cmds/misc/ping.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../../types/discord";
 
 const Ping: SlashCommand = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Check if the bot is online."),
+  data: new SlashCommandBuilder().setName("ping").setDescription("Replies with pong!"),
   async execute(interaction) {
     await interaction.reply({
       ephemeral: true,
